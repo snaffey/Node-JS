debe4d83f1bde78e8db895cabb74bcca09dc58bb
Tiago <ggtiago0santos@gmail.com>
2022-09-13 00:34:12 +0100

    Teste

--- Teste/app.js
+++ Teste/app.js
@@ -1,14 +1,26 @@
 const http = require('http');
+const readline = require('readline');
 
 const hostname = '127.0.0.1';
+
 const port = 3000;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+exports.myDateTime = function () {
+    return Date();
+  };
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.write("Hora:" + exports.myDateTime());
+  res.end();
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Em http://${hostname}:${port}/`);
 });
