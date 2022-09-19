32777178fd6bc563b9ccc2e0cf2d6010a07a16e4
Tiago <ggtiago0santos@gmail.com>
2022-09-19 11:30:57 +0100

    Fim

--- aula4/dist/aula4.dev.js
+++ aula4/dist/aula4.dev.js
@@ -10,6 +10,34 @@ Object.keys(global).forEach(function(value){
     console.log(value);
 });
 */
-Object.keys(process).forEach(function (value) {
-  console.log(value);
+
+/*
+Object.keys(process).forEach(function(value){
+    console.log(value);
 });
+*/
+//console.log(process.argv);
+
+/*
+Process.argv.array.forEach(function(value){
+    console.log(value);
+});
+*/
+var options = process.argv.slice(2);
+
+switch (options[0]) {
+  case 'pid':
+    console.log(process.pid);
+    break;
+
+  case 'arch':
+    console.log(process.arch);
+    break;
+
+  case 'platform':
+    console.log(process.platform);
+    break;
+
+  default:
+    console.log('opção inv');
+}
