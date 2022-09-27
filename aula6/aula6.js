10a7007e7e388cdc81833356bcb05208772b7642
Tiago <ggtiago0santos@gmail.com>
2022-09-27 11:32:52 +0100

    aula7

--- aula6/aula6.js
+++ aula6/aula6.js
@@ -4,9 +4,23 @@ const http = require('http');
 
 const options = {
     hostname: '192.168.40.105',
-    port: 8080,
+    port: 1234,
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
     }
 };
+
+const req = http.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on('data', (d) => {
+        process.stdout.write("Resposta do sv ->" + d);
+    });
+});
+
+req.on('error', (error) => {
+    console.error(error);
+});
+
+req.end();
---
+++ aula7/server.js
@@ -0,0 +1,21 @@
+/*
+Modulos do propio nuclueo do node:
+net - Modulo de interação com o diversos protocolos tcp.
+Deste modo é possivel controlar a transmissão de dados.
+*/
+
+var net = require('net');
+
+var server = net.createServer(function(connection){
+    console.log('Client connected');
+    connection.on('end', function(){
+        console.log('Client disconnected');
+    });
+
+    connection.write('Hello World! Eu sou o servidor\n');
+    connection.pipe(connection);
+});
+
+server.listen(8080, function(){
+    console.log('Server is listening');
+});
